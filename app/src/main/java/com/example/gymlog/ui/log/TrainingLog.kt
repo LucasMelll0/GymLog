2f87bfb4c74aaab3909adb9064a466c5d2eeb41c
LucasMelll0 <lucasmellorodrigues2012@gmail.com>
2023-05-20 20:55:25 -0300

    feature: Tela de detalhes do treino quase terminada.

--- app/src/main/java/com/example/gymlog/ui/log/TrainingLog.kt
+++ app/src/main/java/com/example/gymlog/ui/log/TrainingLog.kt
@@ -21,6 +21,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -31,18 +32,22 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.gymlog.R
 import com.example.gymlog.database.AppDataBase_Impl
-import com.example.gymlog.model.Training
 import com.example.gymlog.repository.TrainingRepositoryImpl
+import com.example.gymlog.ui.components.DefaultAlertDialog
 import com.example.gymlog.ui.components.ExerciseList
 import com.example.gymlog.ui.log.viewmodel.TrainingLogViewModel
 import com.example.gymlog.ui.theme.GymLogTheme
+import com.example.gymlog.utils.BackPressHandler
 import com.example.gymlog.utils.Resource
+import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun TrainingLogScreen(
+    onBackPressed: () -> Unit,
     onNavIconClick: () -> Unit,
     onError: () -> Unit,
+    onClickDelete: () -> Unit,
     trainingId: String,
     modifier: Modifier = Modifier,
     viewModel: TrainingLogViewModel = koinViewModel()
@@ -50,17 +55,53 @@ fun TrainingLogScreen(
     var isLoading: Boolean by remember {
         mutableStateOf(false)
     }
+    var showResetDialog: Boolean by remember {
+        mutableStateOf(false)
+    }
+    var showDeleteDialog: Boolean by remember {
+        mutableStateOf(false)
+    }
     LaunchedEffect(key1 = Unit) {
         viewModel.getTraining(trainingId)
+    }
+    val scope = rememberCoroutineScope()
+
+    BackPressHandler {
+        scope.launch {
+            viewModel.updateTraining(trainingId)
+            onBackPressed()
+        }
     }
 
     Scaffold(bottomBar = {
         TrainingLogBottomAppBar(
-            onNavIconClick = onNavIconClick,
-            onClickDelete = { /*TODO*/ },
-            onClickReset = { /*TODO*/ },
+            onNavIconClick = {
+                scope.launch {
+                    viewModel.updateTraining(trainingId)
+                    onNavIconClick()
+                }
+            },
+            onClickDelete = { showDeleteDialog = true },
+            onClickReset = { showResetDialog = true },
             onClickEdit = { /*TODO*/ })
     }) { paddingValues ->
+        if (showDeleteDialog) {
+            DeleteDialog(onConfirm = {
+                scope.launch {
+                    viewModel.removeTraining(trainingId)
+                    onClickDelete()
+                }
+            }, onDismiss = { showDeleteDialog = false })
+        }
+        if (showResetDialog) {
+            ResetExercisesDialog(
+                onConfirm = {
+                    viewModel.resetExercises()
+                    showResetDialog = false
+                },
+                onDismiss = { showResetDialog = false }
+            )
+        }
         val resource = viewModel.training.collectAsState(Resource.Loading)
         when (resource.value) {
             is Resource.Loading -> {
@@ -85,9 +126,10 @@ fun TrainingLogScreen(
             if (resource.value is Resource.Success) {
                 ExerciseList(
                     modifier = modifier.padding(paddingValues),
-                    exercises = (resource.value as Resource.Success<Training>)
-                        .data.getExercisesWithMutableState(),
-                    onCheckedChange = { _, _ -> })
+                    exercises = viewModel.exercises,
+                    onCheckedChange = { exercise, isChecked ->
+                        viewModel.updateExercise(exercise.id, isChecked)
+                    })
             }
         }
 
@@ -95,25 +137,36 @@ fun TrainingLogScreen(
 
 }
 
-@Suppress("UNCHECKED_CAST")
-@Preview
 @Composable
-private fun TrainingLogScreenPreview() {
-    GymLogTheme {
-        val viewModelFactory = object : ViewModelProvider.NewInstanceFactory() {
-            override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                val repositoryImpl = TrainingRepositoryImpl(AppDataBase_Impl().trainingDao())
-                return TrainingLogViewModel(repositoryImpl) as T
-            }
-        }
-        val viewModel: TrainingLogViewModel = viewModel(factory = viewModelFactory)
-        TrainingLogScreen(
-            onNavIconClick = {},
-            trainingId = "",
-            viewModel = viewModel,
-            onError = {}
-        )
-    }
+private fun DeleteDialog(
+    modifier: Modifier = Modifier,
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit
+) {
+    DefaultAlertDialog(
+        modifier = modifier,
+        title = stringResource(id = R.string.common_dialog_title),
+        text = stringResource(id = R.string.training_log_delete_dialog_text),
+        icon = { Icon(imageVector = Icons.Rounded.Delete, contentDescription = null) },
+        onDismissRequest = onDismiss,
+        onConfirm = onConfirm
+    )
+}
+
+@Composable
+private fun ResetExercisesDialog(
+    modifier: Modifier = Modifier,
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit
+) {
+    DefaultAlertDialog(
+        modifier = modifier,
+        title = stringResource(id = R.string.common_dialog_title),
+        text = stringResource(id = R.string.training_log_reset_dialog_text),
+        icon = { Icon(imageVector = Icons.Rounded.Refresh, contentDescription = null) },
+        onDismissRequest = onDismiss,
+        onConfirm = onConfirm
+    )
 }
 
 @Composable
@@ -149,6 +202,29 @@ private fun TrainingLogBottomAppBar(
             }
         }
     )
+}
+
+@Suppress("UNCHECKED_CAST")
+@Preview
+@Composable
+private fun TrainingLogScreenPreview() {
+    GymLogTheme {
+        val viewModelFactory = object : ViewModelProvider.NewInstanceFactory() {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                val repositoryImpl = TrainingRepositoryImpl(AppDataBase_Impl().trainingDao())
+                return TrainingLogViewModel(repositoryImpl) as T
+            }
+        }
+        val viewModel: TrainingLogViewModel = viewModel(factory = viewModelFactory)
+        TrainingLogScreen(
+            onNavIconClick = {},
+            trainingId = "",
+            viewModel = viewModel,
+            onError = {},
+            onClickDelete = {},
+            onBackPressed = {}
+        )
+    }
 }
 
 @Preview
--- app/src/main/java/com/example/gymlog/ui/log/viewmodel/TrainingLogViewModel.kt
+++ app/src/main/java/com/example/gymlog/ui/log/viewmodel/TrainingLogViewModel.kt
@@ -1,7 +1,9 @@
 package com.example.gymlog.ui.log.viewmodel
 
 import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
+import com.example.gymlog.model.ExerciseMutableState
 import com.example.gymlog.model.Training
 import com.example.gymlog.repository.TrainingRepository
 import com.example.gymlog.utils.Resource
@@ -14,23 +16,55 @@ class TrainingLogViewModel(private val repository: TrainingRepository) : ViewMod
         const val TAG = "trainingViewModel"
     }
 
+    private val _exercises = mutableStateListOf<ExerciseMutableState>()
+    val exercises: List<ExerciseMutableState> get() = _exercises
+
     internal var training: Flow<Resource<Training>> = flow { emit(Resource.Loading) }
         private set
 
     suspend fun getTraining(id: String) {
         training = flow {
             try {
-                repository.getById(id).collect { training ->
-                    training?.let {
-                        emit(Resource.Success(training))
-                    } ?: run {
-                        emit(Resource.Error("Error on get training: null pointer"))
-                    }
+                repository.getById(id)?.let { training ->
+                    emit(Resource.Success(training))
+                    _exercises.clear()
+                    _exercises.addAll(training.getExercisesWithMutableState())
+                } ?: run {
+                    emit(Resource.Error("Error on get training: null pointer"))
                 }
+
             } catch (e: Exception) {
                 Log.w(TAG, "getTraining: ", e)
                 emit(Resource.Error("Error on get training"))
             }
+        }
+    }
+
+    fun updateExercise(exerciseId: String, isChecked: Boolean) {
+        _exercises.find { it.id == exerciseId }?.let {
+            it.isChecked = isChecked
+        }
+    }
+
+    fun resetExercises() {
+        _exercises.forEach {
+            it.isChecked = false
+        }
+    }
+
+    suspend fun removeTraining(trainingId: String) {
+        repository.getById(trainingId)?.let { training ->
+            this.training = flow {
+                emit(Resource.Loading)
+            }
+            repository.remove(training)
+        }
+    }
+
+    suspend fun updateTraining(trainingId: String) {
+        repository.getById(trainingId)?.let { training ->
+            this.training = flow { emit(Resource.Loading) }
+            repository.save(training.copy(exercises = exercises.map { it.toExercise() }))
         }
     }
 
