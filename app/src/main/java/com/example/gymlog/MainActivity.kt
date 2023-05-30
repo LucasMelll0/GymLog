fd3cb780b279723bd8e6e3ee3950e5cd41561867
LucasMelll0 <lucasmellorodrigues2012@gmail.com>
2023-05-30 11:53:22 -0300

    fix: Resolve problema com tarja branca no meio das transições do navigation.

--- app/src/main/java/com/example/gymlog/MainActivity.kt
+++ app/src/main/java/com/example/gymlog/MainActivity.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.gymlog.navigation.AppNavHost
 import com.example.gymlog.ui.theme.GymLogTheme
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
@@ -22,7 +27,12 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GymLogApp() {
     GymLogTheme {
-        val navController = rememberAnimatedNavController()
-        AppNavHost(navController = navController)
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)) {
+            val navController = rememberAnimatedNavController()
+            AppNavHost(navController = navController)
+        }
+
     }
 }
