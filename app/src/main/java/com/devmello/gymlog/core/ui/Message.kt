d8440e32b1e059a3b42376700b0fa081430e4923
Lucas Mello <77343991+LucasMelll0@users.noreply.github.com>
2026-05-15 19:46:34 -0300

    feat(core-ui): add scaffold configuration and management system

--- app/src/main/java/com/devmello/gymlog/core/ui/Message.kt
+++ app/src/main/java/com/devmello/gymlog/core/ui/Message.kt
@@ -1,12 +1,15 @@
 package com.devmello.gymlog.core.ui
 
+import androidx.compose.material3.SnackbarDuration
+
 data class Message(
     val id: Long = System.currentTimeMillis(),
     val text: String,
     val duration: MessageDuration = MessageDuration.SHORT
 )
 
-enum class MessageDuration {
-    SHORT,
-    LONG
+enum class MessageDuration(val snackBarDuration: SnackbarDuration) {
+    SHORT(snackBarDuration = SnackbarDuration.Short),
+    LONG(snackBarDuration = SnackbarDuration.Long),
+    INDEFINITE(snackBarDuration = SnackbarDuration.Indefinite);
 }
--- app/src/main/java/com/devmello/gymlog/core/ui/MessageManager.kt
+++ app/src/main/java/com/devmello/gymlog/core/ui/MessageManager.kt
@@ -1,5 +1,23 @@
 package com.devmello.gymlog.core.ui
 
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
 class MessageManager {
+
+    private val _messages = MutableStateFlow<List<Message>>(emptyList())
+    val messages = _messages.asStateFlow()
+
+    fun postMessage(text: String, duration: MessageDuration = MessageDuration.SHORT) {
+        val message = Message(text = text, duration = duration)
+        _messages.update { currentList -> currentList + message }
+    }
+
+    fun removeMessage(messageId: Long) {
+        _messages.update { currentList ->
+            currentList.filterNot { it.id == messageId }
+        }
+    }
 
 }
